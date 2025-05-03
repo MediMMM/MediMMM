bfb8e7b2abd9469e28d9045179ea699ec7afe111
Mallikarjun Nilangi <mallikarjunnilangi1999@gmail.com>
2025-05-03 18:54:54 +0530

    added api gateway

--- api-gateway/src/main/java/com/mma/gateway/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mma/gateway/api_gateway/ApiGatewayApplication.java
@@ -1,8 +1,7 @@
-package com.wtp;
+package com.mma.gateway.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class ApiGatewayApplication {
--- api-gateway/src/test/java/com/mma/gateway/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/mma/gateway/api_gateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wtp;
+package com.mma.gateway.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
